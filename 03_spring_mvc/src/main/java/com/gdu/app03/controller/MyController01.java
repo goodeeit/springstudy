7e940dd8a8587f70c38b391fb92f4875be8faaaa
goodeeit <gt_min@naver.com>
2023-09-26 15:49:44 +0900

    20230926

--- 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController01.java
+++ 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController01.java
@@ -1,6 +1,8 @@
 package com.gdu.app03.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller  // 컨트롤러를 만드는 Annotation
 public class MyController01 {
@@ -22,9 +24,22 @@ public class MyController01 {
    *  2) URL    : 요청 주소
    */
   
+  // value="/"         : contextPath 요청을 의미한다. http://localhost/app03/ 주소를 의미한다.
+  // value="/main.do"  : /main.do 요청을 의미한다.    http://localhost/app03/main.do 주소를 의미한다.
+  @RequestMapping(value={"/", "/main.do"}, method=RequestMethod.GET)
+  public String welcome() {
+    // ViewResolver의 prefix : /WEB-INF/views/
+    // ViewResolver의 suffix : .jsp
+    return "index";
+  }
   
   
-  
+  @RequestMapping(value="/board/list.do", method=RequestMethod.GET)
+  public String boardList() {
+    // ViewResolver의 prefix : /WEB-INF/views/
+    // ViewResolver의 suffix : .jsp
+    return "board/list";  //   /WEB-INF/views/board/list.jsp
+  }
   
   
   
