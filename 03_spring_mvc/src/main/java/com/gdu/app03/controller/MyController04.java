511070dad9431a6dcaef5377b71fdb0fcbb6d22a
goodeeit <gt_min@naver.com>
2023-09-27 15:37:17 +0900

    20230927

--- 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController04.java
+++ 03_spring_mvc/src/main/java/com/gdu/app03/controller/MyController04.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -59,7 +60,6 @@ public class MyController04 {
   
   
   
-  
   /************************* 세션 정보 삭제하기 *************************/
   
   /*
@@ -85,5 +85,30 @@ public class MyController04 {
   }
   
 
+  
+  /************************* 세션 정보 확인하기 *************************/
+  
+  /*
+   * 1. HttpSession의 getAttribute() 메소드
+   */
+  // @GetMapping("/article/confirm.do")
+  public String confirm(HttpSession session) {
+    
+    String title = (String)session.getAttribute("title");
+    System.out.println(title);
+    return "index";
+    
+  }
+  
+  
+  /*
+   * 2. @SessionAttribute
+   */
+  @GetMapping("/article/confirm.do")
+  public String confirm2(@SessionAttribute("title") String title) {  // session에 저장된 "title" 속성을 String title에 저장한다.
+    System.out.println(title);
+    return "index";
+  }
+  
   
 }
