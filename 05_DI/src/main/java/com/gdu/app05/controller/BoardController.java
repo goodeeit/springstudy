20de261154c53cdb206d780fd8e2f46a1bc35b39
goodeeit <gt_min@naver.com>
2023-10-05 09:51:20 +0900

    20231005

--- 05_DI/src/main/java/com/gdu/app05/controller/BoardController.java
+++ 05_DI/src/main/java/com/gdu/app05/controller/BoardController.java
@@ -8,12 +8,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.gdu.app05.service.BoardService;
 
-@Controller
+@Controller  // 컨트롤러 전용 @Component
 public class BoardController {
   
   @Autowired
   private BoardService boardService;
   
+//  @Autowired  
+//  public BoardController(BoardService boardService) {
+//    super();
+//    this.boardService = boardService;
+//  }
+//
+//  @Autowired
+//  public void setBoardService(BoardService boardService) {
+//    this.boardService = boardService;
+//  }
+
   @RequestMapping(value="/board/list.do", method=RequestMethod.GET)
   public String list(Model model) {
     model.addAttribute("boardList", boardService.getBoardList());
