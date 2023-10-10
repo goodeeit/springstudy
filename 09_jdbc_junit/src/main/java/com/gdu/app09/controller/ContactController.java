9cfd97f92ce1a09a71754a8a40605d8a767b0e70
goodeeit <gt_min@naver.com>
2023-10-10 11:50:51 +0900

    20231010

--- 09_jdbc_junit/src/main/java/com/gdu/app09/controller/ContactController.java
+++ 09_jdbc_junit/src/main/java/com/gdu/app09/controller/ContactController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.gdu.app09.dto.ContactDto;
 import com.gdu.app09.service.ContactService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +23,17 @@ public class ContactController {
     return "contact/list";
   }
   
+  @RequestMapping(value="/contact/write.do", method=RequestMethod.GET)
+  public String write() {
+    return "contact/write";
+  }
   
+  @RequestMapping(value="/contact/add.do", method=RequestMethod.POST)
+  public String add(ContactDto contactDto, RedirectAttributes redirectAttributes) {
+    int addResult = contactService.addContact(contactDto);
+    redirectAttributes.addFlashAttribute("addResult", addResult);
+    return "redirect:/contact/list.do";
+  }
   
   
   
