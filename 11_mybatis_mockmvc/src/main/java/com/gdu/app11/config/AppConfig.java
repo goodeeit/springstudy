5457347201b643781308983a255b22c5c5c8242a
goodeeit <gt_min@naver.com>
2023-10-11 16:55:05 +0900

    20231011

--- 11_mybatis_mockmvc/src/main/java/com/gdu/app11/config/AppConfig.java
+++ 11_mybatis_mockmvc/src/main/java/com/gdu/app11/config/AppConfig.java
@@ -2,26 +2,33 @@ package com.gdu.app11.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.TransactionManager;
 
+@PropertySource(value="classpath:application.properties")
 @EnableAspectJAutoProxy
 @Configuration
 public class AppConfig {
 
+  @Autowired
+  private Environment env;
+  
   // DataSource : CP(Connection Pool)을 처리하는 javax.sql.DataSource 인터페이스
   @Bean
   public DataSource dataSource() {
     DriverManagerDataSource dataSource = new DriverManagerDataSource();  // DriverManagerDataSource : CP(Connection Pool)을 처리하는 스프링 클래스
-    dataSource.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-    dataSource.setUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:xe");
-    dataSource.setUsername("GD");
-    dataSource.setPassword("1111");
+    dataSource.setDriverClassName(env.getProperty("spring.datasource.hikari.driver-class-name"));
+    dataSource.setUrl(env.getProperty("spring.datasource.hikari.jdbc-url"));
+    dataSource.setUsername(env.getProperty("spring.datasource.hikari.username"));
+    dataSource.setPassword(env.getProperty("spring.datasource.hikari.password"));
     return dataSource;
   }
   
