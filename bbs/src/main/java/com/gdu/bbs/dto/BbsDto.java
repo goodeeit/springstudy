4cf87afd8210ce5a062cf383608cd06402c78ce0
goodeeit <gt_min@naver.com>
2023-10-25 21:20:55 +0900

    20231025

---
+++ bbs/src/main/java/com/gdu/bbs/dto/BbsDto.java
@@ -0,0 +1,19 @@
+package com.gdu.bbs.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BbsDto {
+  private int bbsNo;
+  private String editor;
+  private String title;
+  private String contents;
+  private Date createdAt;
+  private Date modifiedAt;
+}
