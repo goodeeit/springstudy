8c4d7d04d33915c7cfa950a82ac3c239b81881fe
goodeeit <gt_min@naver.com>
2023-10-27 15:54:13 +0900

    20231026

--- 15_myhome/src/main/java/com/gdu/myhome/service/BlogServiceImpl.java
+++ 15_myhome/src/main/java/com/gdu/myhome/service/BlogServiceImpl.java
@@ -5,12 +5,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.gdu.myhome.dao.BlogMapper;
 import com.gdu.myhome.dto.BlogDto;
+import com.gdu.myhome.dto.BlogImageDto;
 import com.gdu.myhome.util.MyFileUtils;
 
 import lombok.RequiredArgsConstructor;
@@ -61,12 +66,14 @@ public class BlogServiceImpl implements BlogService {
   
   @Override
   public int addBlog(HttpServletRequest request) {
-    
+
+    // BLOG_T에 추가할 데이터
     String title = request.getParameter("title");
     String contents = request.getParameter("contents");
     int userNo = Integer.parseInt(request.getParameter("userNo"));
     String ip = request.getRemoteAddr();
     
+    // BlogDto 생성
     BlogDto blog = BlogDto.builder()
                     .title(title)
                     .contents(contents)
@@ -74,19 +81,30 @@ public class BlogServiceImpl implements BlogService {
                     .ip(ip)
                     .build();
     
+    // BLOG_T에 추가
+    // BlogMapper의 insertBlog() 메소드를 실행하면
+    // insertBlog() 메소드로 전달한 blog 객체에 blogNo값이 저장된다.
     int addResult = blogMapper.insertBlog(blog);
+    
+    // BLOG 작성시 사용한 이미지 목록 (Jsoup 라이브러리 사용)
+    Document document = Jsoup.parse(contents);
+    Elements elements =  document.getElementsByTag("img");
+    
+    if(elements != null) {
+      for(Element element : elements) {
+        String src = element.attr("src");
+        String filesystemName = src.substring(src.lastIndexOf("/") + 1); 
+        BlogImageDto blogImage = BlogImageDto.builder()
+                                    .blogNo(blog.getBlogNo())
+                                    .imagePath(myFileUtils.getBlogImagePath())
+                                    .filesystemName(filesystemName)
+                                    .build();
+        blogMapper.insertBlogImage(blogImage);
+      }
+    }
     
     return addResult;
     
   }
   
 }
-
-
-
-
-
-
-
-
-
