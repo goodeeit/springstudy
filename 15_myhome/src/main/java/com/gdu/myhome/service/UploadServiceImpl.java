5120152ad9cea7f3828e332933f507c0f3748043
goodeeit <gt_min@naver.com>
2023-11-01 15:53:32 +0900

    20231101

--- 15_myhome/src/main/java/com/gdu/myhome/service/UploadServiceImpl.java
+++ 15_myhome/src/main/java/com/gdu/myhome/service/UploadServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gdu.myhome.service;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -9,12 +10,15 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.gdu.myhome.dao.UploadMapper;
+import com.gdu.myhome.dto.AttachDto;
 import com.gdu.myhome.dto.UploadDto;
 import com.gdu.myhome.dto.UserDto;
 import com.gdu.myhome.util.MyFileUtils;
 import com.gdu.myhome.util.MyPageUtils;
 
 import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
 
 @Transactional
 @RequiredArgsConstructor
@@ -40,9 +44,11 @@ public class UploadServiceImpl implements UploadService {
                                   .build())
                         .build();
     
-//    int addUploadResult = uploadMapper.insertUpload(upload);
+    int addUploadResult = uploadMapper.insertUpload(upload);
     
     List<MultipartFile> files = multipartRequest.getFiles("files");
+    
+    int attachCount = 0;
     
     for(MultipartFile multipartFile : files) {
       
@@ -60,9 +66,29 @@ public class UploadServiceImpl implements UploadService {
         
         multipartFile.transferTo(file);
         
-      }
+        String contentType = Files.probeContentType(file.toPath());  // 이미지의 Content-Type은 image/jpeg, image/png 등 image로 시작한다.
+        int hasThumbnail = (contentType != null && contentType.startsWith("image")) ? 1 : 0;
+        
+        if(hasThumbnail == 1) {
+          File thumbnail = new File(dir, "s_" + filesystemName);  // small 이미지를 의미하는 s_을 덧붙임
+          Thumbnails.of(file)
+                    .size(100, 100)      // 가로 100px, 세로 100px
+                    .toFile(thumbnail);
+        }
+        
+        AttachDto attach = AttachDto.builder()
+                            .path(path)
+                            .originalFilename(originalFilename)
+                            .filesystemName(filesystemName)
+                            .hasThumbnail(hasThumbnail)
+                            .uploadNo(upload.getUploadNo())
+                            .build();
+        
+        attachCount += uploadMapper.insertAttach(attach);
+        
+      }  // if
       
-    }
+    }  // for
     
     return 0;
     
