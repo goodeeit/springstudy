8a963726d3faeb8b54480d36df5313e46c79e4e3
goodeeit <gt_min@naver.com>
2023-11-02 15:54:43 +0900

    20231102

--- movie/src/main/java/com/gdu/movie/controller/MovieController.java
+++ movie/src/main/java/com/gdu/movie/controller/MovieController.java
@@ -1,5 +1,8 @@
 package com.gdu.movie.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gdu.movie.service.MovieService;
@@ -11,5 +14,10 @@ import lombok.RequiredArgsConstructor;
 public class MovieController {
 
   private final MovieService movieService;
+  
+  @GetMapping(value="/searchAllMovies", produces="application/json")
+  public Map<String, Object> searchAllMovies() {
+    return movieService.getMovieList();
+  }
   
 }
--- movie/src/main/java/com/gdu/movie/service/MovieServiceImpl.java
+++ movie/src/main/java/com/gdu/movie/service/MovieServiceImpl.java
@@ -1,8 +1,12 @@
 package com.gdu.movie.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import com.gdu.movie.dao.MovieMapper;
+import com.gdu.movie.dto.MovieDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +15,26 @@ import lombok.RequiredArgsConstructor;
 public class MovieServiceImpl implements MovieService {
 
   private final MovieMapper movieMapper;
+  
+  @Override
+  public Map<String, Object> getMovieList() {
+  
+    int movieCount = movieMapper.getMovieCount();
+    List<MovieDto> list = movieMapper.getMovieList();
+    
+    return Map.of("message", "전체 " + movieCount + "개의 목록을 가져왔습니다."
+                , "list", list
+                , "status", 200);
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 }
